7bfac54d014ce9a6d52b7e4a2999f7bf4093468c
Jeff Martin <bryanjeffrey.martin@capitalone.com>
2017-08-08 13:45:27 -0400

    Functional hide/show nav menu

--- assets/js/2017/nav.js
+++ assets/js/2017/nav.js
@@ -20,9 +20,12 @@ $(function() {
     if (!$('#nav').hasClass('show-expanded-nav')) {
       $('#nav').addClass('show-expanded-nav');
       $('#nav-expanded').addClass('show-expanded-nav');
+      $('#menu-overlay').addClass('show-expanded-nav');
     } else {
+      // TODO Lag removeClass on #nav so it doesn't look like its flashing away?
       $('#nav-expanded').removeClass('show-expanded-nav');
       $('#nav').removeClass('show-expanded-nav');
+      $('#menu-overlay').removeClass('show-expanded-nav');
     }
   }
 
