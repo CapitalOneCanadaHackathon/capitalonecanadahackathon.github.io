429ea459ab235e3f9c9c8a63dd3f695b574878b7
Jeff Martin <bryanjeffrey.martin@capitalone.com>
2017-08-10 16:17:31 -0400

    Animate hero content in

--- assets/js/2017/index.js
+++ assets/js/2017/index.js
@@ -1,4 +1,9 @@
 $(function() {
   // Usage: mosaic(element, highlight, background, numTriangles)
   new mosaic(document.getElementById('hero-mosaic'), [18, 80, 110], [25, 52, 65], 10);
+
+  $('#tagline').addClass('hero-animate-in');
+  $('#event-info').addClass('hero-animate-in');
+  $('#hero-register-cta').addClass('hero-animate-in');
+  $('#learn-more').addClass('hero-animate-in');
 });
